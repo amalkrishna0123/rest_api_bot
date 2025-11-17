a99dadabeb044ca7871c17ae852da9d0fb2985ac
amalkrishna0123 <amalikka0@gmail.com>
2025-11-17 07:33:51 +0400

    Seventh

--- backend/settings.py
+++ backend/settings.py
@@ -174,10 +174,10 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
-    ],
+    ]
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.AllowAny',
+    # ],
 }
 
 
